4cb44a794720d1e1db178a081b4a2b9924065a42
Basselalshawareb <basselalshawareb3@gmail.com>
2024-02-13 23:20:23 +0300

    RotateCenter: overload homography matrix function instead of transform function

--- wrappers/transforms.py
+++ wrappers/transforms.py
@@ -3,7 +3,8 @@ import cv2
 
 from mmdet.structures.bbox import autocast_box_type
 from mmdet.registry import TRANSFORMS
-
+from mmcv.transforms.utils import avoid_cache_randomness, cache_randomness
+from numpy import random
 
 
 @TRANSFORMS.register_module()
@@ -19,37 +20,32 @@ class RotateCenter(RandomAffine):
         translate_matrix_post = self._get_translation_matrix(-trans_x, -trans_y)
         return translate_matrix_pre, translate_matrix_post
 
-    @autocast_box_type()
-    def transform(self, results: dict) -> dict:
-        img = results['img']
-        height = img.shape[0] + self.border[1] * 2
-        width = img.shape[1] + self.border[0] * 2
+    
+    @cache_randomness
+    def _get_random_homography_matrix(self, height, width):
+        # Rotation
+        rotation_degree = random.uniform(-self.max_rotate_degree,
+                                         self.max_rotate_degree)
+        rotation_matrix = self._get_rotation_matrix(rotation_degree)
 
-        warp_matrix = self._get_random_homography_matrix(height, width)
+        # Scaling
+        scaling_ratio = random.uniform(self.scaling_ratio_range[0],
+                                       self.scaling_ratio_range[1])
+        scaling_matrix = self._get_scaling_matrix(scaling_ratio)
+
+        # Shear
+        x_degree = random.uniform(-self.max_shear_degree,
+                                  self.max_shear_degree)
+        y_degree = random.uniform(-self.max_shear_degree,
+                                  self.max_shear_degree)
+        shear_matrix = self._get_shear_matrix(x_degree, y_degree)
+
+        # Translation
         translate_matrix_pre, translate_matrix_post = self._center_coordinates(height, width)
-        warp_matrix_centered = translate_matrix_pre@warp_matrix@translate_matrix_post
-        img = cv2.warpPerspective(
-            img,
-            warp_matrix_centered,
-            dsize=(width, height),
-            borderValue=self.border_val)
-        results['img'] = img
-        results['img_shape'] = img.shape[:2]
 
-        bboxes = results['gt_bboxes']
-        num_bboxes = len(bboxes)
-        if num_bboxes:
-            bboxes.project_(warp_matrix_centered)
-            if self.bbox_clip_border:
-                bboxes.clip_([height, width])
-            # remove outside bbox
-            valid_index = bboxes.is_inside([height, width]).numpy()
-            results['gt_bboxes'] = bboxes[valid_index]
-            results['gt_bboxes_labels'] = results['gt_bboxes_labels'][
-                valid_index]
-            results['gt_ignore_flags'] = results['gt_ignore_flags'][
-                valid_index]
-
-            if 'gt_masks' in results:
-                raise NotImplementedError('RandomAffine only supports bbox.')
-        return results
+        # Sandwishing transformations with translations
+        warp_matrix = shear_matrix @ rotation_matrix @ scaling_matrix
+        warp_matrix_centered = (
+            translate_matrix_pre @ warp_matrix @ translate_matrix_post)
+        
+        return warp_matrix_centered
